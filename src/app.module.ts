cca31be987c0776010ef07cfabc57efc8bac2a48
TimeTREE <timetree@kakao.com>
2024-04-09 13:37:55 +0900

    typeorm mysql 설치 및 docker-compose.yml 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { AppController } from './app.controller';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { WsGateway } from './ws.gateway';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'code_share_user',
+      password: 'code_share_pw',
+      database: 'code_share',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [WsGateway],
 })
