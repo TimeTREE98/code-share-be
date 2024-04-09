f3b4a7e761efa2837ad5134beae2bc71560b5955
TimeTREE <timetree@kakao.com>
2024-04-09 13:15:25 +0900

    swagger 세팅 및 status 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder().setTitle('Code Share API').build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 };
 bootstrap();
