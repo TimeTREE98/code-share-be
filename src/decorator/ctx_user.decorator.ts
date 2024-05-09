46fb5fb3fd4ba4280df7e30f65d901cbca931043
TimeTREE <timetree@kakao.com>
2024-05-09 10:50:34 +0900

    nestjs 코드 전체 삭제

--- src/decorator/ctx_user.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { ExecutionContext, createParamDecorator } from '@nestjs/common';
-
-import { UserDTO } from 'src/data/dto/auth.dto';
-
-export const CtxUser = createParamDecorator((data: any, ctx: ExecutionContext): UserDTO => {
-  const request = ctx.switchToHttp().getRequest();
-
-  return request.user;
-});
