b5fa7fabebb875b8682b5827630d51ffc85f353b
TimeTREE <timetree@kakao.com>
2024-05-09 11:44:03 +0900

    auth route 적용

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
+
+import authRouter from './route/auth.route';
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
-});
+app.use('/auth', authRouter);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
