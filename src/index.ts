70ddcc040f19783419987ab82a5ddf2992fa26d7
TimeTREE <timetree@kakao.com>
2024-05-09 11:29:46 +0900

    prettier 설정 및 tsconfig nestjs 에 맞춰 수정

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, Request, Response } from 'express';
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
 });
 
 app.listen(port, () => {
