b6484da8602d1ee4168ad64474f848b73d0fee6c
TimeTREE <timetree@kakao.com>
2024-05-15 16:26:13 +0900

    dotenv 세팅 및 session auth login 로직 적용

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import { Server, createServer } from 'http';
-import express, { Express, NextFunction, Request, Response } from 'express';
+import express, { Express, NextFunction, Request, Response, json } from 'express';
 
 import authRouter from './routes/auth.route';
 import cors from 'cors';
 import socket from './socket';
 import { Server as socketServer } from 'socket.io';
+import session from 'express-session';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({
+  path: path.resolve(process.cwd(), `.env.${process.env.NODE_ENV}`),
+});
 
 const port = process.env.PORT || 3000;
 
@@ -15,11 +22,20 @@ const io = new socketServer(httpServer, { cors: { origin: '*' } });
 
 socket(io);
 
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET as string,
+    resave: true,
+    saveUninitialized: true,
+  }),
+);
 app.use((req: Request, res: Response, next: NextFunction) => {
   req.io = io;
   next();
 });
 app.use(cors());
+app.use(json());
+
 app.use('/auth', authRouter);
 
 httpServer.listen(port, () => {
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -2,8 +2,20 @@ import express, { Request, Response, Router } from 'express';
 
 const authRouter: Router = express.Router();
 
-authRouter.get('/', (req: Request, res: Response) => {
-  res.send('GET /auth');
+authRouter.get('/me', (req: Request, res: Response) => {
+  if (!req.session.uid) {
+    return res.status(401).json({ status: 'Fail' });
+  }
+  return res.status(200).json({ status: 'Success', data: { uid: req.session.uid } });
+});
+
+authRouter.post('/login', (req: Request, res: Response) => {
+  // TODO: DB connection
+  if (req.body.id === 'admin' && req.body.pw === '1234') {
+    req.session.uid = req.body.id;
+    return res.status(200).json({ status: 'Success' });
+  }
+  return res.status(401).json({ status: 'Fail' });
 });
 
 export default authRouter;
