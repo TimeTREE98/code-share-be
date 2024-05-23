3c8ef595b2c2b6e774bb7b680e03807bf3a69de2
이준 <june@June-MacBookAir.local>
2024-05-23 20:23:29 +0900

    room, file api 수정

--- src/routes/room.route.ts
+++ src/routes/room.route.ts
@@ -2,48 +2,46 @@ import express, { Request, Response, Router } from 'express';
 
 const roomRouter: Router = express.Router();
 
-const room = [
-  {
-    room_num: 1,
-  },
-  {
-    room_num: 2,
-  },
-  {
-    room_num: 3,
-  },
-];
-
-const filelist = [
-  {
-    file: 1,
-    room_idx: 1,
-  },
-  {
-    file: 2,
-    room_idx: 1,
-  },
-  {
-    file: 3,
-    room_idx: 3,
-  },
-];
-
 roomRouter.get('', (req: Request, res: Response) => {
-  return res.status(200).json({ status: 'Success', data: { room: room } });
+  req.dbConn?.execute('SELECT * FROM room', (err, result: any[]) => {
+    if (err !== null) {
+      console.log(err);
+      return res.status(500).json({ status: 'Server Error' });
+    } else {
+      const room_result = [];
+
+      for (var i = 0; i < result.length; i++) {
+        const room = {
+          idx: result[i].idx,
+          name: result[i].name,
+        };
+        room_result.push(room);
+      }
+
+      return res.status(200).json({ status: 'Success', data: { room: room_result } });
+    }
+  });
 });
 
 roomRouter.get('/:room_idx/file', (req: Request, res: Response) => {
-  const room_idx = req.params.room_idx;
-  const file = [];
+  req.dbConn?.execute('SELECT * FROM file WHERE room_idx = ?', [req.params.room_idx], (err, results: any[]) => {
+    if (err !== null) {
+      console.log(err);
+      return res.status(500).json({ status: 'Server Error' });
+    } else {
+      const file_result = [];
 
-  for (var i = 0; i < filelist.length; i++) {
-    if (Number(room_idx) == filelist[i].room_idx) {
-      file.push(filelist[i]);
+      for (var i = 0; i < results.length; i++) {
+        const file = {
+          idx: results[i].idx,
+          name: results[i].name,
+        };
+        file_result.push(file);
+      }
+
+      return res.status(200).json({ status: 'Success', data: { file: file_result } });
     }
-  }
-
-  return res.status(200).json({ status: 'Success', data: { file: file } });
+  });
 });
 
 export default roomRouter;
